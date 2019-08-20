63fc46ec625538b77fe47d201615ac01cbe598fa
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-20 15:55:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,15 @@
 def square_array(numbers)
-  numbers = [1, 2, 3]
+  
+  numbers = ([1, 2, 3])
+  
   counter = 0
+  
   while counter < numbers.length do
-    p numbers[counter]**2
+    
+    puts numbers[counter]**2
+    
     counter += 1
+    
   end
 end
 
