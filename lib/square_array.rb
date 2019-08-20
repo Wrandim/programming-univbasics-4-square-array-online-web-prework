4da82d249625e9f01d1d058188e3138465841f9e
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-20 16:43:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,9 +4,9 @@ def square_array(numbers)
   while counter < numbers.length do
     puts numbers[counter]**2
     counter += 1
+    return "Done"
     end
   end
- square_array(numbers)
  
 
 #def square_array(new_numbers)
