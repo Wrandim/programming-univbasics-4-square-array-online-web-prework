81446b78af1ae8a31717b6a43fce273d406fca15
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-20 16:55:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,11 @@
-def square_array(numbers)
+def square_array(numbers) = squared.new 
   numbers = [1, 2, 3]
   counter = 0
   while counter < numbers.length do
     puts numbers[counter]**2
     counter += 1
-    
-    end
-    return 
+    puts squared
+   end
   end
  
 
